a69756127778fe9e3b4c047ee8aa58a3823d5458
Marcus <marcus.moser@mail.de>
2026-03-01 17:37:39 +0100

    Update: Aktueller Kursstand mit output.txt

--- course/index.js
+++ course/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
-const hello = 'Hello World';
-console.log(hello);
+const textIn = fs.readFileSync('../downloads_course_mat/complete-node-bootcamp-master/1-node-farm/final/txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
